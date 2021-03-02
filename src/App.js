cd622ddc587f0672e4c6c9141dca21fde474a0ce
zakaria-ho-ss <zakariabennan07@gmail.com>
2021-03-02 17:04:13 +0100

    firstpushecommerce

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react'
+import {commerce} from './lib/commerce'
+import {Products, Navbar, Cart} from './componants';
+import { SwapVerticalCircleTwoTone } from '@material-ui/icons';
+
+const App = () => {
+    const [products, setProducts]= useState([]);
+    const [cart, setCart]= useState({});   
+
+    const fetchProducts= async () =>{
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+    const fetchCart = async ()=>{
+        setCart(await commerce.cart.retrieve())
+          
+    }
+    const handleAddToCart = async (productId, quantity)=>{
+
+      const item = await commerce.cart.add(productId,quantity );
+      setCart(item.cart); 
+
+    }
+      useEffect(()=>{
+      fetchProducts(); 
+      fetchCart();
+    },[])
+
+    return (
+        <div>
+            <Navbar totalItems={cart.total_items} />
+          {/* <Products products={products} onAddToCart={handleAddToCart} />  */}
+          <Cart cart ={cart}/>
+        </div>
+    )
+}
+
+export default App
